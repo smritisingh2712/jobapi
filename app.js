fa4efed959f4bc39b5353547eada1ec631ea9d48
Smriti <smritishomya12@gmail.com>
2022-02-16 03:42:43 +0530

    swagger added  job-api

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 /////////////////
+//Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
+
 const express = require('express');
 const app = express();
 //db connection
@@ -41,8 +47,9 @@ app.use(xss())
 
 //routes
 app.get('/', (req, res) => {
-  res.send('jobs api');
+  res.send('<h1>jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
